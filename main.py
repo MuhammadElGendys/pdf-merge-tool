9a1329b237a5a459eff21c11ad57d174e7a63741
MuhammadElGendys <mohammedahmedelgendy@gmail.com>
2026-07-30 18:54:04 +0200

    added some errors for now

--- main.py
+++ main.py
@@ -2,23 +2,37 @@
 from pypdf import PdfReader as PR
 from pypdf import PdfWriter as PW
 
+
 # functions
 def LongSeparetor():
     print("========================================================")
 
+
 def MidSeparetor():
     print("============================")
+
 
 def ShortSeparetor():
     print("==============")
 
-LongSeparetor()
-print("N.B.: The order of your PDFs should be like: 0, 1, 2...")
-LongSeparetor()
 
-user_input_path = input("Please enter the folder's path: ")
-user_input_files_number = int(input("Please enter the PDFs number: "))
+def GetUserInput():
+    global user_input_path
+    user_input_path = input("Please enter the folder's path: ")
+    global user_input_files_number
+    user_input_files_number = int(input("Please enter the PDFs number: "))
 
-# loop to open each single file
-for i in range(user_input_files_number):
-    pass
+
+def ReadingPDFs():
+    # loop to open each single file
+    for i in range():
+        pass
+
+
+def StartingText():
+    LongSeparetor()
+    print("N.B.: The order of your PDFs should be like: 0, 1, 2...")
+    LongSeparetor()
+
+
+GetUserInput()
