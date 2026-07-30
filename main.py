06d48b84f5473b78c8b18ea57abb078ea0479313
MuhammadElGendys <mohammedahmedelgendy@gmail.com>
2026-07-30 18:16:26 +0200

    imported first library and did some error handling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # libraries imported
-import os
-
+from pypdf import PdfReader as PR
+from pypdf import PdfWriter as PW
 
 # functions
 def LongSeparetor():
@@ -9,20 +9,16 @@ def LongSeparetor():
 def MidSeparetor():
     print("============================")
 
-
 def ShortSeparetor():
     print("==============")
-
 
 LongSeparetor()
 print("N.B.: The order of your PDFs should be like: 0, 1, 2...")
 LongSeparetor()
 
 user_input_path = input("Please enter the folder's path: ")
-user_input_files_number = input("Please enter the PDFs number: ")
+user_input_files_number = int(input("Please enter the PDFs number: "))
 
 # loop to open each single file
 for i in range(user_input_files_number):
-    temp_file_concatenation = user_input_path + i + ".pdf"
-    with open(temp_file_concatenation, mode="r", encoding="utf-8") as file:
-        content = file.read()
+    pass
