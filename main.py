eda7daf5642b9200a0375889b68da9f0ffaf1057
MuhammadElGendys <mohammedahmedelgendy@gmail.com>
2026-08-05 02:26:10 +0200

    spacing stuff

--- main.py
+++ main.py
@@ -2,19 +2,15 @@
 from pypdf import PdfReader as PR
 from pypdf import PdfWriter as PW
 
-
 # functions
 def LongSeparetor():
     print("========================================================")
 
-
 def MidSeparetor():
     print("============================")
 
-
 def ShortSeparetor():
     print("==============")
-
 
 def GetUserInput():
     global user_input_path
@@ -22,17 +18,14 @@ def GetUserInput():
     global user_input_files_number
     user_input_files_number = int(input("Please enter the PDFs number: "))
 
-
 def ReadingPDFs():
     # loop to open each single file
     for i in range():
         pass
 
-
 def StartingText():
     LongSeparetor()
     print("N.B.: The order of your PDFs should be like: 0, 1, 2...")
     LongSeparetor()
-
 
 GetUserInput()
